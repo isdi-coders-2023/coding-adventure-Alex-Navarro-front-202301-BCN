f2ebb314be81e1d561878ddd568a9ae6005ed8af
Alex Navarro <alexnavter@gmail.com>
2023-02-27 16:00:14 +0100

    Test Button component

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import Button from "../Button/Button";
+
+const App = (): JSX.Element => {
+  return (
+    <>
+      <p>Hello Developers!</p>
+      <Button text="Sign up" />
+    </>
+  );
+};
+
+export default App;
