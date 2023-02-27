a09d75761789873f5960e9ba801fad9aa179d9cf
Alex Navarro <alexnavter@gmail.com>
2023-02-27 16:37:41 +0100

    Add styles to Button component

---
+++ src/components/Button/ButtonStyled.tsx
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-family: inherit;
+
+  background-color: #cccf18;
+  border-radius: 10px;
+  width: 323px;
+  height: 40px;
+
+  font-family: "Saira Stencil One", sans-serif;
+  font-weight: 400;
+  font-size: 1.25rem;
+  line-height: 31.48px;
+`;
+
+export default ButtonStyled;
