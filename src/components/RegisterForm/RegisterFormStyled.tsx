3a97e184f7134cc419cab6524e780e6dc97a151a
Alex Navarro <alexnavter@gmail.com>
2023-03-01 15:21:44 +0100

    Add styles changes to RegisterFormStyled

--- src/components/RegisterForm/RegisterFormStyled.tsx
+++ src/components/RegisterForm/RegisterFormStyled.tsx
@@ -3,43 +3,36 @@ import styled from "styled-components";
 const RegisterFormStyled = styled.form`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   gap: 48px;
 
-  .form__input {
-    display: flex;
-    height: 40px;
-    width: 325px;
-    border-radius: 8px;
-    border: solid 1px #fff;
-    align-items: center;
-
-    ::placeholder {
-      color: black;
+  .form {
+    &__container {
+      display: flex;
+      flex-direction: column;
+      gap: 9px;
     }
 
-    ::-webkit-file-upload-button {
-      content: "Choose file";
-    }
-  }
-
-  .form__title {
-    font-family: var(--secondary-font);
-    font-size: 20px;
-    color: var(--secondary-color);
-    font-weight: 400;
-    margin-bottom: -25px;
-  }
-
-  .label-container {
-    display: flex;
-    flex-direction: column;
-    gap: 9px;
-
-    &__title {
+    &__label {
       font-family: var(--primary-font);
       font-size: 1.18 rem;
       font-weight: 700;
       color: #fff;
+    }
+
+    &__input {
+      display: flex;
+      height: 40px;
+      max-width: 832px;
+      border-radius: 8px;
+      border: solid 1px #fff;
+      align-items: center;
+      padding: 11px;
+
+      ::placeholder {
+        font-family: var(--primary-font);
+        color: black;
+      }
     }
   }
 `;
